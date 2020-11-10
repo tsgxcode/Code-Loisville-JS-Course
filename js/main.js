dd1a518eb88cbc1072dc344dd7bb4a630fddbca4
TSG <TSG@TSGAC>
2020-11-10 09:48:05 -0500

    11.10.2020 removed some JavaScript, reworking the Fetch

--- js/main.js
+++ js/main.js
@@ -2,26 +2,4 @@
 const seclect = document.getElementById('Artist');
 const seclect = document.getElementById('Year'); 
 //Fetch
-fetch( GET, 'https://api.discogs.com/artists/1?callback=callbackname')
-  .then((Artist) => {
-          callbackname({
-              "meta": {
-                  "status": 200,
-              },
-              "data": {
-                  "id": Artist,
-                  "name": ""
-              }
-          })
-      })    
-      .then((Year) => {
-        callbackname({
-            "meta": {
-                "status": 200,
-            },
-            "data": {
-                "id": Year,
-                "year": ""
-            }
-        })
-    })    
+fetch()
