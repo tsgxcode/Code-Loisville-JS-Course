4de705a107302ae0cd62741f2b81196caf136a80
Scott George <tsgcats@gmail.com>
2020-11-11 18:05:14 -0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-//Variables
-const artist = document.getElementById('Artist');
-const year = document.getElementById('Year');
-const form = document.querySelector('form'); 
-
-//Fetch
-fetch('https://www.discogs.com/search/?q=Vinyl&type=all')
-    .then(response => console.log(response))
